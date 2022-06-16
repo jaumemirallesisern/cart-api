3756d417bf8d0ea36fadeda13f02eadd58d1ca6d
Jaume Miralles Isern <info@circusofthings.com>
2022-06-16 13:22:41 +0200

    Refactored data model comments

--- server/routes.ts
+++ server/routes.ts
@@ -8,8 +8,7 @@ export default function routes(app: Application): void {
 
   // Let's consider that we have another resource in our REST called "Quote", 
   // not just an operation on Products. It will be more scalable.
-  // It might not be persistent in DB in the future but it may be,
-  // and possibly we will be accessing the same service as products in code.
+  // It might not be persistent in DB in the future but it may be.
   // A Quote is build from a list of indexed Products with its quantities, let's call it "ChartItem", 
   // coming in the request's payload, that we can call Cart-order or list.
   // It assumes Cart primary decission is hold in Front End side. It's OK in most of the cases
@@ -19,10 +18,10 @@ export default function routes(app: Application): void {
   // The data model could be something like this by now. 
   // Please note that I'm not specifying relational keys and inner fields 
   // as we are not dealing with real DB model.
-  // Product  ---> (1:N)  --->   QuoteItem   ---> (N:1)  --->   Quote
-  //          ---> (1:N)  --->   QuoteItemWithOffers   ---> (N:1)  --->   QuoteWithOffers
-  //                                                   ---> (1:N)  --->   Offer
-  // Product  ---> (1:N)  --->   ChartItem   ---> (N:1)  --->   ChartOrder
+  // Product  ---> (1:1)  --->   QuoteItem   ---> (N:1)  --->   Quote
+  //          ---> (1:1)  --->   QuoteItemWithOffers   ---> (N:1)  --->   QuoteWithOffers
+  //                                                   ---> (1:1)  --->   Offer
+  // Product  ---> (1:1)  --->   ChartItem   ---> (N:1)  --->   ChartOrder
   // We wouldn't have many-to-many relations and the need for Join tables.
   app.use('/api/v1/quotes', quotesRouter);  
 
